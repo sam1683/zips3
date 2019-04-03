4fb64c000f69b8eb5fed779ddb7e5674d232277d
sam1683 <xam.16.83@gmail.com>
2019-04-03 17:00:44 -0500

    Require

--- zips3/lambda.js
+++ zips3/lambda.js
@@ -1,8 +1,20 @@
+/**
+ * 
+ * SJP
+ * 
+ */
+
+let AWS = require('aws-sdk');
+let JSZip = require("jszip");
+let fs = require("fs");
+const s3 = new AWS.S3();
 
 
 exports.handler = function(event, context, callback) {
     
-
     
     callback(null, {"message": "Successfully executed"});
 }
+
+let changes = event.changes;
+let modified = 0, removed = 0;
